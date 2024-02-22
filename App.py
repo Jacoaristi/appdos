e0ba543636adaebc68b49ba7beb84feb8db74e56
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-02-22 08:36:31 -0500

    Update App.py

--- App.py
+++ App.py
@@ -10,3 +10,14 @@ st.title("Interfaces Multimodales.")
 image = Image.open('INTELESTELAR.jpg')
 
  st.image(image, width=200)                 
+
+
+try:
+  os.mkdir("temp")
+except:
+  pass
+
+st.subheader("Texto a audio.")
+st.write ('Las interfaces de texto a audio son fundamentales en las interfaces multimediales ya que permiten'
+         'una comunicacion mas accesible y natural,facilitando la inclusion de personas con discapacidades'
+         )
