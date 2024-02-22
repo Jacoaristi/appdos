32dacfb158ae841376d597f6ee06582df9ec364f
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-02-22 08:27:49 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import os
 import time
+import glob
 import os
 from gtts import gTTs
 from PIL import Image
 
+st.title("Interfaces Multimodales.")
+image = Image.open('text_to_audio.png)
+
+ st.image(image, width=200)                 
